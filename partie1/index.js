3145ab497971bf4afeb7f9340f40bbe0a9ff5f53
Rodos06D <betumlawson@gmail.com>
2023-08-08 12:34:52 +0200

    1er partie les variable

---
+++ partie1/index.js
@@ -0,0 +1,11 @@
+let nom; //declaration de la variable
+let prenom, age;
+ // affectation de variable
+let nomEtudiant="Valentin";
+const nombreDeVie= 100;
+console.log(nom,nomEtudiant,nombreDeVie);
+nom= prompt("Quel est votre nom?");
+prenom= prompt("Quel est votre prenom?");
+age= prompt("Quel est votre age?");
+alert("Mineur");
+console.log("Bienvenu" + nom, prenom, age); 
