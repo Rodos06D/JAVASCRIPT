76f6ccc50169336a742572748484db7b861fb0c0
Rodos06D <betumlawson@gmail.com>
2023-08-08 13:08:41 +0200

    2em sauvergade consernant les conditions

--- partie1/index.js
+++ partie1/index.js
@@ -1,5 +1,5 @@
-let nom; //declaration de la variable
-let prenom, age;
+//let nom; //declaration de la variable
+/*let prenom, age;
  // affectation de variable
 let nomEtudiant="Valentin";
 const nombreDeVie= 100;
@@ -8,4 +8,18 @@ nom= prompt("Quel est votre nom?");
 prenom= prompt("Quel est votre prenom?");
 age= prompt("Quel est votre age?");
 alert("Mineur");
-console.log("Bienvenu" + nom, prenom, age); 
+console.log("Bienvenu" + nom, prenom, age); */
+let nombre;
+nombre= Number(prompt("entre un nombre")
+
+console.log(typeof nombre);
+if(nombre<0)
+{
+    console.log("Ce nombre est negatif")
+}
+else if(nombre>0){
+    console.log("Ce nombre est positif")
+}
+else{
+    console.log("Ce nombre est nulle")
+}
